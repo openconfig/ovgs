aacf43e25bc1890059420796f089704bf343a1f0
mojiiba <mbagherz@cisco.com>
2023-11-17 01:17:55 -0500

    use compile_protos.sh instead of generate-proto.sh

--- compile_protos.sh
+++ compile_protos.sh
@@ -1,26 +1,23 @@
-#!/bin/bash
-
-# Copyright 2023 Google Inc.
+#
+# Copyright 2023 Google LLC All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#    http:#www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+#!/bin/bash
+set -euo pipefail
 
 
-if [ -z $SRCDIR ]; then
-	THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-	SRC_DIR=${THIS_DIR}/..
-fi
-
-
-cd ${SRC_DIR}
-protoc -I${SRC_DIR}  --go-grpc_out=. --go-grpc_opt=paths=source_relative --go_out=. --go_opt=paths=source_relative ${SRC_DIR}/ovgs.proto
+# Go
+protoc --go-grpc_out=. --go-grpc_opt=paths=source_relative --go_out=. --go_opt=paths=source_relative ovgs.proto
 
